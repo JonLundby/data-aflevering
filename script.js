780c07fcfcc8d1b7d441a53de60ed16be6523958
Jon <lundbyjon@hotmail.com>
2023-03-24 00:18:49 +0100

    dialog/modal view issues
    404

--- script.js
+++ script.js
@@ -5,10 +5,9 @@ window.addEventListener("load", initApp);
 async function initApp() {
   console.log("app initialized...");
 
-  const pokemon = await getPokeMon("/trevenant.json");
-  console.log(pokemon);
-  showpokemons(pokemon);
-  console.log(pokemon.image);
+  const trevenant = await getPokeMon("https://raw.githubusercontent.com/JonLundby/data-aflevering/main/trevenant.json");
+  console.log(trevenant);
+  showpokemons(trevenant);
 }
 
 async function getPokeMon(url) {
@@ -20,8 +19,8 @@ async function getPokeMon(url) {
 
 function showpokemons(pokemon) {
   console.log("showing pokemons...");
-  
-    const pokemonsTable = /*html*/ `
+
+  const pokemonsTable = /*html*/ `
     <tbody>
       <tr>
         <td>${pokemon.name}</td>
@@ -37,44 +36,43 @@ function showpokemons(pokemon) {
   document.querySelector("#pokemon-table").insertAdjacentHTML("beforeend", pokemonsTable);
 
   document.querySelector("#pokemon-table tbody:last-child").addEventListener("click", viewPokemon);
-  document.querySelector("#dialog-close").addEventListener("click", closeView);
 
   function viewPokemon(pokemon) {
     console.log("view pokemon clicked...");
 
-    // const dialogListHTML = /*html*/ `
-    //   <img src="${pokemon.image}">
-    //   <ul>
-    //     <li>Name: ${pokemon.name}</li>
-    //     <li>Name: ${pokemon.generation}</li>
-    //     <li>Name: ${pokemon.spilversion}</li>
-    //     <li>Name: ${pokemon.statsHp}</li>
-    //     <li>Name: ${pokemon.statsAttack}</li>
-    //     <li>Name: ${pokemon.statsDefence}</li>
-    //     <li>Name: ${pokemon.statsSpecialAttack}</li>
-    //     <li>Name: ${pokemon.statsSpecialDefence}</li>
-    //     <li>Name: ${pokemon.statsSpeed}</li>
-    //   </ul>
-    // `;
-
-    //document.querySelector("#dialog-viewPokemon").insertAdjacentHTML("beforeend", dialogListHTML);
-
-    document.querySelector("#dialog-image").src = pokemon.image;
-    document.querySelector("#dialog-name").textContent = pokemon.name;
-    document.querySelector("#dialog-generation").textContent = pokemon.generation;
-    document.querySelector("#dialog-spilversion").textContent = pokemon.spilversion;
-    document.querySelector("#dialog-statsHP").textContent = pokemon.statsHp;
-    document.querySelector("#dialog-statsAttack").textContent = pokemon.statsAttack;
-    document.querySelector("#dialog-statsDefence").textContent = pokemon.statsDefence;
-    document.querySelector("#dialog-statsSpecialAttack").textContent = pokemon.statsSpecialAttack;
-    document.querySelector("#dialog-statsSpecialDefence").textContent = pokemon.statsSpecialDefence;
-    document.querySelector("#dialog-statsSpeed").textContent = pokemon.statsSpeed;
-
+    const dialogListHTML = /*html*/ `
+      <ul>
+        <img src="${pokemon.image}">
+        <li>Name: ${pokemon.name}</li>
+        <li>Name: ${pokemon.generation}</li>
+        <li>Name: ${pokemon.spilversion}</li>
+        <li>Name: ${pokemon.statsHp}</li>
+        <li>Name: ${pokemon.statsAttack}</li>
+        <li>Name: ${pokemon.statsDefence}</li>
+        <li>Name: ${pokemon.statsSpecialAttack}</li>
+        <li>Name: ${pokemon.statsSpecialDefence}</li>
+        <li>Name: ${pokemon.statsSpeed}</li>
+      </ul>
+    `;
     document.querySelector("#dialog-viewPokemon").showModal();
+
+    document.querySelector("#dialog-viewPokemon").insertAdjacentHTML("beforeend", dialogListHTML);
+    // document.querySelector("#dialog-close").addEventListener("click", closeView);
+
+    // document.querySelector("#dialog-image").src = pokemon.image;
+    // document.querySelector("#dialog-name").textContent = pokemon.name;
+    // document.querySelector("#dialog-generation").textContent = pokemon.generation;
+    // document.querySelector("#dialog-spilversion").textContent = pokemon.spilversion;
+    // document.querySelector("#dialog-statsHP").textContent = pokemon.statsHp;
+    // document.querySelector("#dialog-statsAttack").textContent = pokemon.statsAttack;
+    // document.querySelector("#dialog-statsDefence").textContent = pokemon.statsDefence;
+    // document.querySelector("#dialog-statsSpecialAttack").textContent = pokemon.statsSpecialAttack;
+    // document.querySelector("#dialog-statsSpecialDefence").textContent = pokemon.statsSpecialDefence;
+    // document.querySelector("#dialog-statsSpeed").textContent = pokemon.statsSpeed;
   }
 
-  function closeView() {
-    console.log("pokemon view closed...");
-    document.querySelector("#dialog-viewPokemon").close();
-  }
+  // function closeView() {
+  //   console.log("pokemon view closed...");
+  //   document.querySelector("#dialog-viewPokemon").close();
+  // }
 }
