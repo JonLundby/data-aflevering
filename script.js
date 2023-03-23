f2aa8f1a31fd8819b8816c152c90546d4d866498
Jon <lundbyjon@hotmail.com>
2023-03-23 17:32:49 +0100

    dialog window broken...

--- script.js
+++ script.js
@@ -8,28 +8,54 @@ async function initApp() {
   console.log("app initialized...");
 
   const pokemon = await getPokeMon("trevenant.json");
-
+  console.log(pokemon);
   showpokemons(pokemon);
   
 }
 
 async function getPokeMon(url) {
   const response = await fetch(url);
-  const data = response.json();
+  const data = await response.json();
 
   return data
 }
 
 function showpokemons(pokemon) {
-  document.querySelector("#pokemon-table tbody").insertAdjacentHTML("beforeend", /*html*/ `
+  console.log("showing pokemons...");
+  document.querySelector("#pokemon-table").insertAdjacentHTML("beforeend", /*html*/ `
     <tr>
       <td>${pokemon.name}</td>
       <td><img src="${pokemon.image}"</td>
-      <td>${pokemon.spilversion}</td>
       <td>${pokemon.generation}</td>
+      <td>${pokemon.spilversion}</td>
       <td>${pokemon.height}</td>
       <td>${pokemon.weight}</td>
       <td>${pokemon.gender}</td>
     </tr>
   `);
+
+  document.querySelector("#pokemon-table tbody:last-child").addEventListener("click", viewPokemon);
+  document.querySelector("#dialog-close").addEventListener("click", closeView)
+
+  function viewPokemon(pokemon) {
+    console.log("view pokemon clicked...");
+
+    document.querySelector("#dialog-image").src = pokemon.image;
+    document.querySelector("#dialog-name").textContent = pokemon.name;
+    document.querySelector("#dialog-generation").textContent = pokemon.generation;
+    document.querySelector("#dialog-spilversion").textContent = pokemon.spilversion;
+    document.querySelector("#dialog-statsHP").textContent = pokemon.statsHp;
+    document.querySelector("#dialog-statsAttack").textContent = pokemon.statsAttack;
+    document.querySelector("#dialog-statsDefence").textContent = pokemon.statsDefence;
+    document.querySelector("#dialog-statsSpecialAttack").textContent = pokemon.statsSpecialAttack;
+    document.querySelector("#dialog-statsSpecialDefence").textContent = pokemon.statsSpecialDefence;
+    document.querySelector("#dialog-statsSpeed").textContent = pokemon.statsSpeed;
+
+    document.querySelector("#dialog-viewPokemon").showModal();
+  }
+
+  function closeView() {
+    console.log("pokemon view closed...");
+    document.querySelector("#dialog-viewPokemon").close();
+  }
 }
